535cc35fbbc892c48b2690456e6f9e33e75e2a92
Krishnaveni_Boominathan <you@example.com>
2023-04-07 09:40:54 +0530

    added

--- task3/index.php
+++ task3/index.php
@@ -61,6 +61,7 @@
         
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
     <script>
         // $(document).ready(function(){
         //   $("#myForm").submit(function(event){
@@ -86,6 +87,13 @@
         //   });
 
         $(document).ready(function(){
+            $('#myForm').validate({
+                rules:{
+                    fname: "required"
+                }, message:{
+                    fname: "Please enter your name"
+                }
+            });
         $('.drive').click(function(){
         var selectedValue = $(this).val();
         if(selectedValue == 'yes'){
